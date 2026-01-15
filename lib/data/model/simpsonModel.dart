1fc39e2511ae6cc1998a7e65a14465d765f84a6b
trusty <ivanbordeira2015@hotmail.com>
2026-01-15 13:52:23 -0300

    refactoring

---
+++ lib/data/model/simpsonModel.dart
@@ -0,0 +1,8 @@
+class Simpsonmodel {
+    final String name;
+  final int? age;
+  final String gender;
+  final List<String> phrases;
+
+  Simpsonmodel({required this.name, required this.age, required this.gender, required this.phrases});
+}
--- lib/data/model/simpsonsResponse.dart
+++ lib/data/model/simpsonsResponse.dart
@@ -1,17 +1,11 @@
 class Simpsonsmodel {
   final List<dynamic> results;
-  final String name;
-  final int age;
-  final String gender;
-  final List<String> phrases;
 
-  Simpsonsmodel({required this.name, required this.results, required this.age, required this.gender, required this.phrases});
+
+  Simpsonsmodel({required this.results});
 
   factory Simpsonsmodel.fromJson(Map<String, dynamic> json){
-    return Simpsonsmodel(results: json["results"], 
-    name: json["name"], 
-    age: json["age"], 
-    gender: json["gender"], 
-    phrases: json["phrases"]);
+    return Simpsonsmodel(results: json["results"]);
+    
   }
 }
